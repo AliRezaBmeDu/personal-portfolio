39b7c4158e0bd132216523e3f8fd164990b48b10
AliReza <ali.reza.buet@gmail.com>
2023-09-23 03:17:45 +0600

    Add multiple container and components

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+
+import { About, Footer, SKills, Testimonial, Header, Work, Skills } from './container';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
-    <h1>App</h1>
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   )
 }
 
