4e8526e36b93a595c474700f9bfcf2c7c7eed479
AliReza <ali.reza.buet@gmail.com>
2023-09-23 04:15:20 +0600

    Add hamburger menu and functionalities

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React ,{ useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
 import './Navbar.scss';
 
 import { images } from '../../constants';
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -19,6 +23,26 @@ const Navbar = () => {
           ) )
         }
       </ul>
+
+      <div className='app__navbar-menu'>
+        <HiMenuAlt4 onClick={()=> setToggle(true)}/>
+        {toggle && (
+          <motion.div
+            whileInView={{x:[300, 0]}}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            {
+              ['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`}>{item}</a>
+                </li>
+          ) )
+        }
+          </motion.div>
+        )}
+      </div>
+      
     </nav>
   )
 }
