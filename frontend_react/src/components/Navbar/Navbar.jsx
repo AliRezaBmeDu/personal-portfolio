0cc1f435a56153d73dc59a38ab2d1dae072e8e24
AliReza <ali.reza.buet@gmail.com>
2023-09-23 03:44:58 +0600

    Check the assets workability. working

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -9,6 +9,16 @@ const Navbar = () => {
       <div>
         <img src={images.logo} alt="logo" />
       </div>
+      <ul>
+        {
+          ['home', 'about', 'contact', 'work', 'skills'].map((item) => (
+            <li key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ) )
+        }
+      </ul>
     </nav>
   )
 }
