983f605c805056dadb9b999503a9c7875416388f
AliReza <ali.reza.buet@gmail.com>
2023-09-24 03:27:46 +0600

    Connect with the sanity

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -8,7 +8,18 @@ import { urlFor, client } from '../../client';
 
 const Work = () => {
   const [activeFilter, setActiveFilter] = useState('All');
-  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 })
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type=="works"]';
+    client.fetch(query)
+      .then((data) => {
+        setWorks(data);
+        setFilterWork(data);
+      })
+  }, [])
   
   const handleWorkFilter = (item) => {
 
@@ -31,8 +42,18 @@ const Work = () => {
         ))}
       </div>
       
-      <motion.div>
-          
+      <motion.div
+          animate={animateCard}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className='app__work-portfolio'
+      >
+          {filterWork.map((work,index) => (
+            <div className='app__work-item app__flex' key={index}>
+              <div className='app__work-img app__flex'>
+                <img src={urlFor(work.imgUrl)} alt={work.name} />
+              </div>
+            </div>
+          ))}
       </motion.div>
 
     </React.Fragment>
