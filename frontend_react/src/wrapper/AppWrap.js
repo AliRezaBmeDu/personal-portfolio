0eacc1c473d79cad801cd4cd122a11cea49485c4
AliReza <ali.reza.buet@gmail.com>
2023-09-24 02:28:16 +0600

    Add copyright

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -8,7 +8,14 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
 
         <div className='app__wrapper app__flex'>
             <Component />
+
+            <div className='copyright'>
+                <p className='p-text'>@2023 AliReza</p>
+                <p className='p-text'>All rights reserved</p>
+            </div>
         </div>
+        
+        <NavigationDots active={idName} />
     </div>
   )
 }
