d3902a77a8041214cc518aaeda27d87b714669a6
AliReza <ali.reza.buet@gmail.com>
2023-09-24 03:14:06 +0600

    Add work section layout

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 import './Work.scss';
 
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
+
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 })
+  
+  const handleWorkFilter = (item) => {
+
+  }
+
   return (
-    <div>Work</div>
+    <React.Fragment>
+      <h2 className='head-text'>
+        My Creative <span>Portfolio</span> Section
+      </h2>
+      <div className='app__work-filter'>
+        {['UI/UX', 'Web App', 'React JS', 'All'].map((item,index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : "" }`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+      
+      <motion.div>
+          
+      </motion.div>
+
+    </React.Fragment>
   )
 }
 
