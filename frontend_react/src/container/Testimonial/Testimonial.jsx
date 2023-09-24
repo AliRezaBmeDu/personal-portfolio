2037e9bcf977fd52e0112f01f6953fc96d8aad92
AliReza <ali.reza.buet@gmail.com>
2023-09-25 02:54:04 +0600

    Fix the button design for testimonials

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Testimonial.scss';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
 const Testimonial = () => {
   const [brands, setBrands] = useState([]);
@@ -23,13 +24,33 @@ const Testimonial = () => {
       setBrands(data);
     })
   }, [])
+
+  const test = testimonials[currentIndex];
+
+  const handleClick = (index) => {
+    setCurrentIndex(index);
+  }
   return (
     <React.Fragment>
       {testimonials.length && (
         <React.Fragment>
           <div className='app__testimonial-item app__flex'>
-          {console.log('testimonials', testimonials)}
-          <img src={urlFor(testimonials[currentIndex].imgurl)} alt="testimonals" />
+            <img src={urlFor(test.imgurl)} alt="testimonals" />
+            <div className='app__testimonial-content'>
+              <p className='p-text'>{test.feedback}</p>
+              <div>
+                <h4 className='bold-text'>{test.name}</h4>
+                <h5 className='p-text'>{test.company}</h5>
+              </div>
+            </div>
+            </div>
+          <div className='app__testimonial-btns app__flex'>
+              <div className='app__flex' onClick={()=> handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+                <HiChevronLeft />
+              </div>
+              <div className='app__flex' onClick={()=> handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+                <HiChevronRight />
+              </div>
           </div>
         </React.Fragment>
       )}
