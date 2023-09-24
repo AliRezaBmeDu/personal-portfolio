a49af834e1a1235b05627499b0ed26d82bbb47d9
AliReza <ali.reza.buet@gmail.com>
2023-09-25 03:47:40 +0600

    Add the submit functions

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -6,6 +6,25 @@ import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.input;
+    setFormData({...formData, [name]: value})
+  }
+
+  const handleSubmit = () => {
+    setLoading(true);
+  }
+  
   return (
     <React.Fragment>
       <h2 className='head-text'>If you need any assistance to develop your website, please don't hesitate to contact me</h2>
@@ -21,8 +40,21 @@ const Footer = () => {
       <div className='app__footer-form app__flex'>
         <div className='app__flex'>
           <input className='p-text' type='text' placeholder='Your Name' name='name' value={name} onChange={handleChangeInput} />
-          
         </div>
+        <div className='app__flex'>
+          <input className='p-text' type='email' placeholder='Your Email' name='email' value={email} onChange={handleChangeInput} />
+        </div>
+        <div>
+          <textarea
+            className='p-text'
+            placeholder='Your Message'
+            value={message}
+            name={message}
+            onChange={handleChangeInput}
+          >
+          </textarea>
+        </div>
+        <button type='button' className='p-text' onClick={handleSubmit}>{loading ? 'sending' : 'send message'}</button>
       </div>
     </React.Fragment>
   )
