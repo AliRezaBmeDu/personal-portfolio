0980dafee6eaf05506f05945df4834a383478858
AliReza <ali.reza.buet@gmail.com>
2023-09-24 17:27:38 +0600

    Add experience layout

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import ReactToolTip from 'react-tooltip';
+import ReactTooltip from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -42,6 +42,31 @@ const Skills = () => {
               <p className='p-text'>{skill.name}</p>
             </motion.div>
           ))}
+        </motion.div>
+        <motion.div className='app__skills-exp'>
+            {experience.works.map((work) => (
+              <React.Fragment>
+                <motion.div
+                  whileInView={{ opacity: [0, 1] }}
+                  transition= {{ duration: 0.5 }}
+                  className='app__skills-exp-work'
+                  data-tip
+                  data-for={work.name}
+                  key={work.name}
+                >
+                  <h4 className='bold-text'>{work.name}</h4>
+                  <p className='p-text'>{work.company}</p>
+                </motion.div>
+                <ReactTooltip
+                  id={work.name}
+                  effect='solid'
+                  arrowColor="#fff"
+                  className="skills-tooltip"
+                >
+                  {work.desc}
+                </ReactTooltip>
+              </React.Fragment>
+            ))}
         </motion.div>
       </div>
     </React.Fragment>
